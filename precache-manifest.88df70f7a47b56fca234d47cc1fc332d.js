a8719c712733217683cc11d574bd1a5a6f841c59
Ben Friedland <ben174@gmail.com>
2020-06-01 16:57:38 -0700

    Updates

---
+++ precache-manifest.88df70f7a47b56fca234d47cc1fc332d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c39d1e5a04c15777ab2146fa8bd24c7a",
+    "url": "/busy-boy/index.html"
+  },
+  {
+    "revision": "0d9268821dca73200a5f",
+    "url": "/busy-boy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "52ba17eda1509cae2f5e",
+    "url": "/busy-boy/static/js/2.05fe9d19.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/busy-boy/static/js/2.05fe9d19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d9268821dca73200a5f",
+    "url": "/busy-boy/static/js/main.0811fff6.chunk.js"
+  },
+  {
+    "revision": "689258aa276851a66668",
+    "url": "/busy-boy/static/js/runtime-main.6d07bab6.js"
+  }
+]);
